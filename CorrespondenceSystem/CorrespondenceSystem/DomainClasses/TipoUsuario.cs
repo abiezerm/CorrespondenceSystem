5f0e7a3daa034eccb48046cc766969a89dfdb882
unknown <k.batista@TECNOLOGIA.Solidaridad.local>
2015-07-13 12:54:30 -0400

    TipoUsuario added

---
+++ CorrespondenceSystem/CorrespondenceSystem/DomainClasses/TipoUsuario.cs
@@ -0,0 +1,9 @@
+﻿namespace CorrespondenceSystem.DomainClasses
+{
+    public class TipoUsuario
+    {
+
+        public virtual int idTipoUsuario { get; set; }
+        public virtual string descripcion { get; set; }
+    }
+}
---
+++ CorrespondenceSystem/CorrespondenceSystem/MappingClasses/TipoUsuarioMap.cs
@@ -0,0 +1,15 @@
+﻿using CorrespondenceSystem.DomainClasses;
+using FluentNHibernate.Mapping;
+
+namespace CorrespondenceSystem.MappingClasses
+{
+    public class TipoUsuarioMap : ClassMap<TipoUsuario>
+    {
+        public TipoUsuarioMap()
+        {
+            Id(a => a.idTipoUsuario);
+            Map(a => a.descripcion);
+            
+        }
+    }
+}
