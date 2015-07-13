b8821ed64b85faf9294f27441769bb0ca15d251f
unknown <k.batista@TECNOLOGIA.Solidaridad.local>
2015-07-13 11:34:45 -0400

    Mensajero's Classes created

---
+++ CorrespondenceSystem/CorrespondenceSystem/MappingClasses/MensajeroMap.cs
@@ -0,0 +1,14 @@
+﻿using CorrespondenceSystem.DomainClasses;
+using FluentNHibernate.Mapping;
+
+namespace CorrespondenceSystem.MappingClasses
+{
+    public class MensajeroMap : ClassMap<Mensajero>
+    {
+        public MensajeroMap()
+        {
+            Id(a => a.idMensajero);
+            Map(a => a.descripcion);
+        }
+    }
+}
