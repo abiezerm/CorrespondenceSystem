7745fe6998cfd7e635648d0a3bea051874c25a0f
unknown <k.batista@TECNOLOGIA.Solidaridad.local>
2015-07-13 12:39:17 -0400

    TipoDepartamentoMap Checked

--- CorrespondenceSystem/CorrespondenceSystem/MappingClasses/TipoDepartamentoMap.cs
+++ CorrespondenceSystem/CorrespondenceSystem/MappingClasses/TipoDepartamentoMap.cs
@@ -1,14 +1,14 @@
-﻿using FluentNHibernate.Mapping;
+﻿using CorrespondenceSystem.DomainClasses;
+using FluentNHibernate.Mapping;
 
 namespace CorrespondenceSystem.MappingClasses
 {
-    public class TipoDepartamentoMap : ClassMap<DOMAIN_CLASS>
+    public class TipoDepartamentoMap : ClassMap<TipoDepartamento>
     {
         public TipoDepartamentoMap()
         {
-            Id(a => a.Id_attribute);
-            Map(a => a.Value_to_Map);
-            
+            Id(a => a.idTipoDepartamento);
+            Map(a => a.descripcion);
         }
     }
 }
