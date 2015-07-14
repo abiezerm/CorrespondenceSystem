fec27545ca91e4d96043ab5b0c7732c428294356
unknown <k.batista@TECNOLOGIA.Solidaridad.local>
2015-07-14 13:43:43 -0400

    Documento Model added

---
+++ CorrespondenceSystem/CorrespondenceSystem/DomainClasses/Documento.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CorrespondenceSystem.DomainClasses
+{
+    public class Documento
+    {
+        public virtual int idDocumento { get; set; }
+        public virtual string codigo { get; set; }
+        public virtual DateTime fechaCreacionUsuario { get; set; }
+        public virtual DateTime fechaRegistroUsuario { get; set; }
+        public virtual string asunto { get; set; }
+        public virtual Departamento departamento { get; set; }
+        public virtual DateTime fechaCreacion { get; set; }
+        public virtual DateTime fechaModificacion { get; set; }
+        public virtual Usuario usuario { get; set; }
+
+        public Documento()
+        {
+            departamento = new Departamento();
+            usuario = new Usuario();
+        }
+    }
+}
---
+++ CorrespondenceSystem/CorrespondenceSystem/MappingClasses/DocumentoMap.cs
@@ -0,0 +1,21 @@
+﻿using CorrespondenceSystem.DomainClasses;
+using FluentNHibernate.Mapping;
+
+namespace CorrespondenceSystem.MappingClasses
+{
+    public class DocumentoMap : ClassMap<Documento>
+    {
+        public DocumentoMap()
+        {
+            Id(a => a.idDocumento);
+            Map(a => a.codigo).Not.Nullable();
+            Map(a => a.fechaCreacion).Not.Nullable();
+            Map(a => a.fechaRegistroUsuario).Not.Nullable();
+            Map(a => a.asunto).Not.Nullable();
+            References(a => a.departamento).Column("idDepartamento").Not.Nullable();
+            References(a => a.usuario, "usuarioCreacion").Column("idUsuario").Not.Nullable();
+            References(a => a.usuario, "usuarioModificacion").Column("idUsuario");
+
+        }
+    }
+}
