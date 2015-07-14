fccf382c291bce5ee448d52b08de89d0934d3e16
unknown <k.batista@TECNOLOGIA.Solidaridad.local>
2015-07-14 11:44:01 -0400

    Usuario Model added

---
+++ CorrespondenceSystem/CorrespondenceSystem/DomainClasses/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CorrespondenceSystem.DomainClasses
+{
+    public class Usuario
+    {
+        public virtual int idUsuario { get; set; }
+        public virtual string nombre { get; set; }
+        public virtual string correo { get; set; }
+        public virtual TipoUsuario tipoUsuario { get; set; }
+        public virtual DateTime fechaRegistro { get; set; }
+        public virtual DateTime fechaModificacion { get; set; }
+        public virtual int usuarioCreacion { get; set; }
+        public virtual int usuarioModificacion { get; set; }
+
+        public Usuario()
+        {
+            tipoUsuario = new TipoUsuario();
+        }
+    }
+}
---
+++ CorrespondenceSystem/CorrespondenceSystem/MappingClasses/UsuarioMap.cs
@@ -0,0 +1,20 @@
+﻿using CorrespondenceSystem.DomainClasses;
+using FluentNHibernate.Mapping;
+
+namespace CorrespondenceSystem.MappingClasses
+{
+    public class UsuarioMap : ClassMap<Usuario>
+    {
+        public UsuarioMap()
+        {
+            Id(a => a.idUsuario);
+            Map(a => a.nombre).Not.Nullable();
+            Map(a => a.correo).Not.Nullable();
+            References(a => a.tipoUsuario).Column("idTipoUsuario");
+            Map(a => a.fechaRegistro).Not.Nullable();
+            Map(a => a.fechaModificacion);
+            Map(a => a.usuarioCreacion).Not.Nullable();
+            Map(a => a.usuarioModificacion);
+        }
+    }
+}
